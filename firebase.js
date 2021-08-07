cbd19e901e0d1a13d0764d3847f49f380ac3b2d6
Colin Edwards <ColinJhEdwards@gmail.com>
2021-08-07 12:16:23 -0700

    added firebase sdk to scripts

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDopg-dxSnWs4OkQIYQktj2eQI7RoXIttc",
+  authDomain: "clone-a8b68.firebaseapp.com",
+  projectId: "clone-a8b68",
+  storageBucket: "clone-a8b68.appspot.com",
+  messagingSenderId: "369096865922",
+  appId: "1:369096865922:web:c34edd773615fb30af1c9b",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
