5b3f1ff3db8ca40a444bc0d6fe55552754a996ea
Colin Edwards <ColinJhEdwards@gmail.com>
2021-08-07 19:14:27 -0700

    looped over each item in store db and displayed content on home page

--- script.js
+++ script.js
@@ -13,7 +13,62 @@ function getItems() {
           price: doc.data().price,
         });
       });
-      console.log(items);
+      generateItems(items);
     });
+}
+
+function generateItems(items) {
+  let itemsHTML = "";
+  items.forEach((item) => {
+    itemsHTML += `
+     <div class="product mt-4">
+     <div
+       class="
+         image
+         w-60
+         h-56
+         bg-white
+         flex
+         justify-center
+         items-center
+         rounded-lg
+       "
+     >
+       <img
+         class="p-4"
+         src=${item.image}
+         alt="switch"
+       />
+     </div>
+     <div class="name text-gray-700 font-bold mt-2 text-sm">
+       ${item.name}
+     </div>
+     <div class="make text-green-700 font-bold">${item.make}</div>
+     <div class="rating text-yellow-300 font-bold my-1">
+       ⭐⭐⭐⭐⭐ ${item.rating}
+     </div>
+     <div class="price font-bold text-gray-700 text-lg">$${item.price}.00</div>
+     <div
+       class="
+         add-cart-btn
+         h-8
+         w-28
+         bg-yellow-500
+         flex
+         justify-center
+         items-center
+         rounded
+         mt-1
+         text-white
+         cursor-pointer
+         hover:bg-yellow-600
+       "
+     >
+       Add to Cart
+     </div>
+   </div>
+     `;
+  });
+  document.querySelector(".products").innerHTML = itemsHTML;
 }
 getItems();
